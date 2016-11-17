8d57f1b622f7a20b896f764b9d064e27e6691b81
Felix Konstantin Maurer <maufl@maufl.de>
2016-11-17 15:25:43 +0100

    Propagate errors from message unmarshaling

--- protocol/ofp10/parse.go
+++ protocol/ofp10/parse.go
@@ -11,62 +11,63 @@ func Parse(b []byte) (message util.Message, err error) {
 	switch b[1] {
 	case Type_Hello:
 		message = new(ofpxx.Header)
-		message.UnmarshalBinary(b)
+		err = message.UnmarshalBinary(b)
 	case Type_Error:
 		message = new(ErrorMsg)
-		message.UnmarshalBinary(b)
+		err = message.UnmarshalBinary(b)
 	case Type_EchoRequest:
 		message = new(ofpxx.Header)
-		message.UnmarshalBinary(b)
+		err = message.UnmarshalBinary(b)
 	case Type_EchoReply:
 		message = new(ofpxx.Header)
-		message.UnmarshalBinary(b)
+		err = message.UnmarshalBinary(b)
 	case Type_Vendor:
 		message = new(VendorHeader)
-		message.UnmarshalBinary(b)
-	 case Type_FeaturesRequest:
+		err = message.UnmarshalBinary(b)
+	case Type_FeaturesRequest:
 		message = NewFeaturesRequest()
-		message.UnmarshalBinary(b)
-	 case Type_FeaturesReply:
+		err = message.UnmarshalBinary(b)
+	case Type_FeaturesReply:
 		message = NewFeaturesReply()
-		message.UnmarshalBinary(b)
+		err = message.UnmarshalBinary(b)
 	case Type_GetConfigRequest:
 		message = new(ofpxx.Header)
-		message.UnmarshalBinary(b)
+		err = message.UnmarshalBinary(b)
 	case Type_GetConfigReply:
 		message = new(SwitchConfig)
-		message.UnmarshalBinary(b)
+		err = message.UnmarshalBinary(b)
 	case Type_SetConfig:
 		message = NewSetConfig()
-		message.UnmarshalBinary(b)
+		err = message.UnmarshalBinary(b)
 	case Type_PacketIn:
 		message = new(PacketIn)
-		message.UnmarshalBinary(b)
+		err = message.UnmarshalBinary(b)
 	case Type_FlowRemoved:
 		message = NewFlowRemoved()
-		message.UnmarshalBinary(b)
+		err = message.UnmarshalBinary(b)
 	case Type_PortStatus:
 		message = new(PortStatus)
-		message.UnmarshalBinary(b)
+		err = message.UnmarshalBinary(b)
 	case Type_PacketOut:
-		break
+		message = new(PacketOut)
+		err = message.UnmarshalBinary(b)
 	case Type_FlowMod:
 		message = NewFlowMod()
-		message.UnmarshalBinary(b)
+		err = message.UnmarshalBinary(b)
 	case Type_PortMod:
 		break
 	case Type_StatsRequest:
 		message = new(StatsRequest)
-		message.UnmarshalBinary(b)
+		err = message.UnmarshalBinary(b)
 	case Type_StatsReply:
 		message = new(StatsReply)
-		message.UnmarshalBinary(b)
-	 case Type_BarrierRequest:
+		err = message.UnmarshalBinary(b)
+	case Type_BarrierRequest:
 		message = new(ofpxx.Header)
-		message.UnmarshalBinary(b)
-	 case Type_BarrierReply:
+		err = message.UnmarshalBinary(b)
+	case Type_BarrierReply:
 		message = new(ofpxx.Header)
-		message.UnmarshalBinary(b)
+		err = message.UnmarshalBinary(b)
 	case Type_QueueGetConfigRequest:
 		break
 	case Type_QueueGetConfigReply:
